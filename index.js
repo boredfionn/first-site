8dd3753c1a915b58c37db61acdec4c771d178a6d
boredfionn <133231278+boredfionn@users.noreply.github.com>
2023-05-12 18:36:20 +0300

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log (1);
+const button = document.querySelector('button');
+const output = document.querySelector ('.output');
+button.addEventListener('click', function() {
+output.innerHTML = 'распродано 😢'});
